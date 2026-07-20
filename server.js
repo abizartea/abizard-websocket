ddf320959b2b411075de1b4b0afab77846ef798b
abizartea <abyytea86@gmail.com>
2026-07-20 17:39:55 +0700

    update websocket server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
 
 const PORT = 8159;
 
@@ -11,6 +14,31 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log("Abizard Widgets jalan di port " + PORT);
+// WebSocket Server
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+
+    console.log("Client terhubung");
+
+    ws.on("message", (message) => {
+
+        console.log("Data:", message.toString());
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        });
+
+    });
+
+    ws.on("close", () => {
+        console.log("Client keluar");
+    });
+
+});
+
+server.listen(PORT, () => {
+    console.log("ABIZARD berjalan di http://localhost:" + PORT);
 });
